30b786efe5f00d92ef042574550350f8d9b3dab6
RaphaelNagato <raphaelnagato19@gmail.com>
2023-02-28 23:29:55 +0000

    push image to docker image and run kubernetes cluster

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="raphaelnagato/ml_predict"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-predict-kube --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward ml-predict-kube 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ml_predict
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $dockerpath:latest"
+docker login --username=raphaelnagato
 
 # Step 3:
 # Push image to a docker repository
+docker push raphaelnagato/$dockerpath:latest
+
